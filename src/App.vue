89e008dcdad29db0f6bf6413b4097e6f2000e555
marciock <marciockalves@gmail.com>
2020-07-15 12:53:54 -0300

    view

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+
---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div>
+       <div class="row mt-5">
+           <div class="col-sm-8">
+            <div class="card shadow p-3 mb-5 bg-white rounded" >
+                <div class="card-body">
+                    <h5 class="card-title ">Login</h5>
+                    <div class=" card-subtitle">
+                        <div class="form-group ">
+                            <label for="email">Email</label>
+                            <input type="mail" id="email"  class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">senha</label>
+                            <input type="password" id="password"  class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group float-right">
+                        <a href="#" class="card-link">Cadastrar</a>
+                        <button type="button" class="btn btn-outline-success ml-3" >Entrar</button>
+                    </div>
+                </div>
+            </div>
+           </div>
+       </div>
+        
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:'Login'
+}
+</script>
+<style >
+
+.row{
+    margin-left: 10em;
+}
+
+</style>
